13da5450bb5c252fab8f3b70e47776e92b5df407
Guilherme Braga <guilhermebragariosdacosta@gmail.com>
2023-11-30 20:17:21 -0300

    DAO and MODEL complete update 2

--- src/DAO/Pedidos_produtosDAO.java
+++ src/DAO/Pedidos_produtosDAO.java
@@ -5,11 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-<<<<<<< HEAD
 import Model.Pedidos_produtos;
-=======
 import Model.Usuarios;
->>>>>>> feed0cbab4b2cc04ef265814c049eea8b9e84de2
 
 public class Pedidos_produtosDAO {
 
@@ -23,7 +20,6 @@ public class Pedidos_produtosDAO {
 		this.connection = connection;
 	}
 
-<<<<<<< HEAD
 	public void inserirPedidos_produtos(Pedidos_produtos pp) throws SQLException {
 
 		String sql = "INSERT INTO fiado_pago.pedidos_produtos (pedido_id, produto_id, quantidade) VALUES (?, ?, ?)";
@@ -35,9 +31,6 @@ public class Pedidos_produtosDAO {
 		preparedStatement.executeUpdate();
 	}
 
-	public ResultSet listarPedidos_Produtos() throws SQLException {
-		String sql = "SELECT * FROM fiado_pago.pedidos_produtos";
-=======
 	public void inserirUsuario(Usuarios user) throws SQLException {
 
 		String sql = "INSERT INTO Usuarios (usuario_id, email, senha, emailrecup, telefone) VALUES (?, ?, ?, ?, ?)";
@@ -46,31 +39,12 @@ public class Pedidos_produtosDAO {
 		preparedStatement.setInt(1, user.getUsuario_id());
 		preparedStatement.setString(2, user.getEmail());
 		preparedStatement.setString(3, user.getSenha());
-		preparedStatement.setString(4, user.getEmailRecup());
-		preparedStatement.setString(5, user.getSenha());
-		preparedStatement.executeUpdate();
-	}
-
-	public void atualizarUsuario(Usuarios user) throws SQLException {
-		String sql = "UPDATE Usuarios SET senha = ?, emailrecup = ?, telefone = ? WHERE usuario_id = ?";
-		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-		preparedStatement.setString(1, user.getSenha());
-		preparedStatement.setString(2, user.getEmailRecup());
-		preparedStatement.setString(3, user.getTelefone());
-		preparedStatement.setInt(4, user.getUsuario_id());
-		preparedStatement.executeUpdate();
-	}
-
-	public void excluirUsuario(Usuarios user) throws SQLException {
-		String sql = "DELETE FROM Usuarios WHERE usuario_id = ?";
-		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-		preparedStatement.setInt(1, user.getUsuario_id());
+		preparedStatement.setString(5, user.getTelefone());
 		preparedStatement.executeUpdate();
 	}
 
 	public ResultSet listarUsuarios() throws SQLException {
-		String sql = "SELECT * FROM Usuarios";
->>>>>>> feed0cbab4b2cc04ef265814c049eea8b9e84de2
+		String sql = "SELECT * FROM fiado_pago.usuarios";
 		PreparedStatement preparedStatement = connection.prepareStatement(sql);
 		return preparedStatement.executeQuery();
 	}
--- src/Model/Pedidos_produtos.java
+++ src/Model/Pedidos_produtos.java
@@ -21,24 +21,19 @@ public int getPedido_id () {
 	return pedido_id;
 }
 
-<<<<<<< HEAD
 public void setPedido_id (int pedido_id) {
 	this.pedido_id = pedido_id;
 }
 
-=======
->>>>>>> feed0cbab4b2cc04ef265814c049eea8b9e84de2
 public int getProduto_id () {
 	return produto_id;
 }
 
-<<<<<<< HEAD
+
 public void setProduto_id (int produto_id) {
 	this.produto_id = produto_id;;
 }
 
-=======
->>>>>>> feed0cbab4b2cc04ef265814c049eea8b9e84de2
 public int getQuantidade () {
 	return quantidade;
 }
