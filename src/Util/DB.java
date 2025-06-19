41385bed4e32187319e60d8f5eae8b9c0c67aef3
guiibrag4 <guilhermebragaepic2@gmail.com>
2025-06-19 17:24:21 -0300

    Admin: remoção de arquivos de compilação e alteração de variáveis de segurança

--- src/Util/DB.java
+++ src/Util/DB.java
@@ -1,5 +1,6 @@
 package Util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
@@ -32,11 +33,43 @@ public class DB {
 		}
 	}
 
+	// private static Properties loadProperties() {
+	// try (FileInputStream fs = new FileInputStream("db.properties")) {
+	// Properties props = new Properties();
+	// props.load(fs);
+	// return props;
+	// } catch (IOException e) {
+	// throw new DbException(e.getMessage());
+	// }
+	// }
+
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
-			Properties props = new Properties();
-			props.load(fs);
+		Properties props = new Properties();
+		try {
+			// Carrega as variáveis do arquivo .env se ele existir
+			File envFile = new File(".env");
+			if (envFile.exists()) {
+				props.load(new FileInputStream(envFile));
+			}
+
+			// Sobrescreve com variáveis de ambiente do sistema, se existirem
+			// Isso dá prioridade às variáveis do sistema (útil em produção)
+			String dbUser = System.getenv("DB_USER");
+			String dbPassword = System.getenv("DB_PASSWORD");
+			String dbUrl = System.getenv("DB_URL");
+
+			if (dbUser != null)
+				props.setProperty("user", dbUser);
+			if (dbPassword != null)
+				props.setProperty("password", dbPassword);
+			if (dbUrl != null)
+				props.setProperty("dburl", dbUrl);
+
+			// Adiciona a propriedade useSSL que não é secreta
+			props.setProperty("useSSL", "false");
+
 			return props;
+
 		} catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
